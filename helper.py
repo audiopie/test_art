efceebeee108a2024054be67beb1e66b17885a10
audiopie <surroundyou@ya.ru>
2020-11-01 14:55:51 +0300

    add exchange rate method

--- helper.py
+++ helper.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import xml.etree.ElementTree as ET
 from urllib.request import urlopen
 
@@ -20,3 +21,39 @@ def get_list_currency():
             row = {value.text: key.text}
             results.append(row)
     return results
+
+
+def parse_id():
+    root = get_root()
+    item_ids = root.findall('./Item')
+    codes = root.findall('.//ISO_Char_Code')
+    currency_values = {}
+    for k, v in zip(item_ids, codes):
+        for i in k.attrib.values():
+            currency_values[v.text] = i
+    return currency_values
+
+
+def get_day(date, code, data):
+    item_id = ''
+    currency_date = {'date': date, 'currency_rate': None}
+    for key, value in data.items():
+        if key == code:
+            item_id = value
+            break
+    year, month, day = date.split('-')
+    try:
+        response = ET.parse(urlopen(f'http://www.cbr.ru/scripts/XML_daily.asp?date_req={day}/{month}/{year}'))
+        root = response.getroot()
+        try:
+            currency = root.find(f"./Valute[@ID='{item_id}']/Value").text.replace(',', '.')
+            currency_date['date'] = date
+            currency_date['currency_rate'] = currency
+            return currency_date
+        except AttributeError:
+            print("No Attribute")
+        finally:
+            return currency_date
+    except ET.ParseError:
+        print('Error')
+
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask
-from flask import jsonify
-from helper import get_list_currency
+from flask import jsonify, request
+from helper import get_list_currency, get_day, parse_id
 
 app = Flask(__name__)
 
@@ -8,6 +8,25 @@ app = Flask(__name__)
 @app.route('/currencies/api/v1.0/', methods=['GET'])
 def get_currencies_list():
     return jsonify(get_list_currency())
+
+
+# http://127.0.0.1:5000/exchange/api/v1.0/?date1=2020-10-30&date2=2020-09-01&code=EUR
+@app.route('/exchange/api/v1.0/', methods=['GET'])
+def exchange_rate_differential():
+    date1 = request.args.get('date1', type=str)
+    date2 = request.args.get('date2', type=str)
+    code = request.args.get('code', type=str)
+    if date1 == '' or date2 == '' or code == '':
+        return jsonify("One param of empty")
+
+    else:
+        get_currency_id = parse_id()
+        currency_date1 = get_day(date1, code, get_currency_id)
+        currency_date2 = get_day(date2, code, get_currency_id)
+        rate = float(currency_date1['currency_rate']) - float(currency_date2['currency_rate'])
+        difference = f'Difference currency {code} relative to the RUB between ' \
+                     f'date {date1} and date {date2} is {rate} RUB'
+        return jsonify(currency_date1, currency_date2, difference)
 
 
 if __name__ == "__main__":
