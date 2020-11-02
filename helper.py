d8faf937833c87676baafe1f1f370c405a204f6e
audiopie <surroundyou@ya.ru>
2020-11-02 18:58:55 +0300

    refactor: rewrite helper to class

--- helper.py
+++ helper.py
@@ -3,79 +3,72 @@ from urllib.request import urlopen
 import xml.etree.ElementTree as ET
 
 
-def get_root():
-    """ Отправляем запрос на страницу бонка и парсим с помощью библиотеки xml.etree."""
-    try:
-        url = urlopen('http://www.cbr.ru/scripts/XML_valFull.asp')
-        return ET.parse(url)
-    except ET.ParseError:
-        return 'Probably the page not found'
+class CurrencyApi:
 
+    def __init__(self):
+        self.url = urlopen('http://www.cbr.ru/scripts/XML_valFull.asp')
+        self.list_of_currencies = ET.parse(self.url)
+        self.names = self.list_of_currencies.findall('.//Name')
+        self.codes = self.list_of_currencies.findall('.//ISO_Char_Code')
+        self.ids = self.list_of_currencies.findall('./Item')
+        self.list_result = []
+        self.currency_values = {}
 
-def get_list_currency():
-    """ Парсим дом дерево и находим имя валют, код, сохраняем в массив."""
-    root = get_root()
-    names = root.findall('.//Name')
-    codes = root.findall('.//ISO_Char_Code')
-    results = []
-    for key, value in zip(names, codes):
-        if value.text:
-            row = {value.text: key.text}
-            results.append(row)
-    return results
+    def get_list_currency(self):
+        """ Парсим дом дерево и находим имя валют, код, сохраняем в массив."""
+        for key, value in zip(self.names, self.codes):
+            if value.text:
+                row = {value.text: key.text}
+                self.list_result.append(row)
+        return self.list_result
 
+    def parse_id(self):
+        """ Парсим дом дерево и находим ID  валют, код."""
+        for k, v in zip(self.ids, self.codes):
+            for i in k.attrib.values():
+                self.currency_values[v.text] = i
+        return self.currency_values
 
-def parse_id():
-    """ Парсим дом дерево и находим ID  валют, код."""
-    root = get_root()
-    item_ids = root.findall('./Item')
-    codes = root.findall('.//ISO_Char_Code')
-    currency_values = {}
-    for k, v in zip(item_ids, codes):
-        for i in k.attrib.values():
-            currency_values[v.text] = i
-    return currency_values
-
-
-def get_day(date, code):
-    """ Обрабатываем данные и отправляем запрос на получение значения курса за определенный день
-        Возвращает словарь с курсом валюты и датой.
-    """
-    currency_date = {'date': date, 'currency_rate': None}
-    year, month, day = date.split('-')
-    try:
-        response = ET.parse(urlopen(f'http://www.cbr.ru/scripts/XML_daily.asp?date_req={day}/{month}/{year}'))
-        root = response.getroot()
-        try:
-            currency = root.find(f"./Valute[@ID='{code}']/Value").text.replace(',', '.')
-            currency_date['date'] = date
-            currency_date['currency_rate'] = currency
-            return currency_date
-        except AttributeError:
-            return "No Attribute"
-        finally:
-            return currency_date
-    except ET.ParseError as e:
-        return e
-
-
-def validate_date(date):
-    """ Валидации даты, например 2020-02-30 не является корректной датой"""
-    try:
+    def get_day(self, date, code):
+        """ Обрабатываем данные и отправляем запрос на получение значения курса за определенный день
+            Возвращает словарь с курсом валюты и датой.
+        """
+        currency_date = {'date': date, 'currency_rate': None}
         year, month, day = date.split('-')
-        return datetime(int(year), int(month), int(day))
-    except ValueError as e:
-        return False
+        try:
+            response = ET.parse(urlopen(f'http://www.cbr.ru/scripts/XML_daily.asp?date_req={day}/{month}/{year}'))
+            root = response.getroot()
+            try:
+                currency = root.find(f"./Valute[@ID='{code}']/Value").text.replace(',', '.')
+                currency_date['date'] = date
+                currency_date['currency_rate'] = currency
+                return currency_date
+            except AttributeError:
+                return "No Attribute"
+            finally:
+                return currency_date
+        except ET.ParseError as e:
+            return e
 
+    def validate_date(self, date):
+        """ Валидации даты, например 2020-02-30 не является корректной датой"""
+        try:
+            year, month, day = date.split('-')
+            return datetime(int(year), int(month), int(day))
+        except ValueError as e:
+            return False
 
-def check_code(code):
-    """ Валидируем код, не должен быть меньше трех символов и должен быть в списке валют"""
-    codes = parse_id()
-    if len(code) == 3:
-        for key, value in codes.items():
-            if key == code:
-                return value
+    def check_code(self, code):
+        """ Валидируем код, не должен быть меньше трех символов и должен быть в списке валют"""
+        if self.currency_values:
+            codes = self.currency_values
+        else:
+            codes = self.parse_id()
+        if len(code) == 3:
+            for key, value in codes.items():
+                if key == code:
+                    return value
+            else:
+                return False
         else:
             return False
-    else:
-        return False
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import jsonify, request
-from helper import get_list_currency, get_day, validate_date, check_code
+from helper import CurrencyApi
 
 app = Flask(__name__)
 
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/currencies/api/v1.0/', methods=['GET'])
 def get_currencies_list():
     """ Метод возвращает список валюты и наименование валют """
-    return jsonify(get_list_currency())
+    return jsonify(helperApi.get_list_currency())
 
 
 @app.route('/exchange/api/v1.0/', methods=['GET'])
@@ -20,10 +20,10 @@ def exchange_rate_differential():
     date1 = request.args.get('date1', type=str)
     date2 = request.args.get('date2', type=str)
     code = request.args.get('code', type=str).upper()
-    valid_code = check_code(code)
-    if validate_date(date1) and validate_date(date2) and valid_code:
-        currency_date1 = get_day(date1, valid_code)
-        currency_date2 = get_day(date2, valid_code)
+    valid_code = helperApi.check_code(code)
+    if helperApi.validate_date(date1) and helperApi.validate_date(date2) and valid_code:
+        currency_date1 = helperApi.get_day(date1, valid_code)
+        currency_date2 = helperApi.get_day(date2, valid_code)
         rate = float(currency_date1['currency_rate']) - float(currency_date2['currency_rate'])
         difference = f'Difference currency {code} relative to the RUB between ' \
                      f'date {date1} and date {date2} is {rate} RUB'
@@ -33,4 +33,5 @@ def exchange_rate_differential():
 
 
 if __name__ == "__main__":
+    helperApi = CurrencyApi()
     app.run(debug=False)
